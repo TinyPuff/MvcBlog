fb66e64f528202f10f97c00fb1d366e0d6dcddd6
TinyPuff <ghost1of1king@gmail.com>
2025-01-26 21:08:01 +0330

    migrations for CustomUsername

---
+++ Migrations/20250126172939_BlogUserCustomUsername.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlogUserCustomUsername : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomUsername",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomUsername",
+                table: "AspNetUsers");
+        }
+    }
+}
