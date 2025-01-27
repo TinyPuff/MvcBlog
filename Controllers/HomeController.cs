6218399e25996e4c9c10f3dc4c3cb4515c33f2c9
TinyPuff <ghost1of1king@gmail.com>
2025-01-27 21:42:35 +0330

    showing posts in homepage via ViewModel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,52 @@
+using System.Data.Common;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MvcBlog.Models;
+using MvcBlog.Models.ViewModels;
+using System.Linq;
+using MvcBlog.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcBlog.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly BlogDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, BlogDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        /* var allPosts = (from post in db.
+                       select new PostVM
+                       {
+                            ID = post.ID,
+                            Title = post.Title,
+                            Body = post.Body,
+                            CreatedAt = post.CreatedAt,
+                            UpdatedAt = post.UpdatedAt,
+                            Author = post.Author
+                       })
+                       .OrderByDescending(p => p.ID)
+                       .ToList(); */
+        var posts = _context.Post
+                    .Include(p => p.Author)
+                    .OrderByDescending(p => p.CreatedAt)
+                    .ToList();
+
+        var postsVM = new PostVM
+        {
+            Posts = posts
+        };
+
+        Console.WriteLine($"Posts count: {posts.Count}");
+        Console.WriteLine($"VM Posts count: {postsVM.Posts.Count}");
+        return View(postsVM);
     }
 
     public IActionResult Privacy()
---
+++ Models/ViewModels/PostVM.cs
@@ -0,0 +1,8 @@
+using MvcBlog.Data;
+
+namespace MvcBlog.Models.ViewModels;
+
+public class PostVM
+{
+    public List<Post> Posts { get; set; }
+}
