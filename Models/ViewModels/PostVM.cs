8815c37823231070d05e8b10047044edeb1a607d
TinyPuff <ghost1of1king@gmail.com>
2025-01-30 14:46:39 +0330

    removed Comments nav property from post view model

--- Models/ViewModels/PostVM.cs
+++ Models/ViewModels/PostVM.cs
@@ -15,6 +15,4 @@ public class PostVM
     [Required]
     [StringLength(400, ErrorMessage = "Text must have less than 400 characters.")]
     public string Body { get; set; } = string.Empty;
-
-    public ICollection<Comment>? Comments { get; set; }
 }
